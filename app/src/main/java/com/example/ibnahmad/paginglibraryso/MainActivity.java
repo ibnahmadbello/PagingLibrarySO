e67698be847f7def3ca4f87d0fdf6384f07d81c1
ibnahmad <justforlearningcode@gmail.com>
2018-12-20 21:50:09 +0100

    Link MainActivity to LiveData

--- app/src/main/java/com/example/ibnahmad/paginglibraryso/MainActivity.java
+++ app/src/main/java/com/example/ibnahmad/paginglibraryso/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.ibnahmad.paginglibraryso;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.arch.paging.PagedList;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // getting recyclerview
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setting up recyclerview
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        // getting our ItemViewModel
+        ItemViewModel itemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
+
+        // creating the adapter
+        final ItemAdapter itemAdapter = new ItemAdapter(this);
+
+        // observing the itempagedlist from view model
+        itemViewModel.itemPagedList.observe(this, new Observer<PagedList<Item>>() {
+            @Override
+            public void onChanged(@Nullable PagedList<Item> items) {
+                // in case of any changes
+                // submitting the items to adapter
+                itemAdapter.submitList(items);
+            }
+        });
+
+        // setting the adapter
+        recyclerView.setAdapter(itemAdapter);
     }
 }
